0c186c600ce483151528ae43873599f6cec2960c
chrisdoc <christoph@meedoc.com>
2016-10-20 13:44:23 +0200

    add toolbar to main activity

--- app/src/main/java/xyz/husten/finstergram/finstergrams/FinsterGridActivity.java
+++ app/src/main/java/xyz/husten/finstergram/finstergrams/FinsterGridActivity.java
@@ -1,8 +1,10 @@
 package xyz.husten.finstergram.finstergrams;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import javax.inject.Inject;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,11 +15,14 @@ import xyz.husten.finstergram.api.InstagramApi;
 import xyz.husten.finstergram.model.SearchResult;
 import xyz.husten.finstergram.utils.ActivityUtils;
 
-public class FinsterGridActivity extends Activity {
+public class FinsterGridActivity extends AppCompatActivity {
   @Inject FinstergramsPresenter presenter;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_finster_grid);
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
     FinsterGridFragment gridFragment =
         (FinsterGridFragment) getFragmentManager().findFragmentById(R.id.content_frame);
